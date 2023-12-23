0121c3c75cba7a942304bbf68ecd8b4ce5fed860
unknown <weck2505@gmail.com>
2023-12-23 23:02:13 +0700

    final

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const sqlite3 = require("sqlite3").verbose();
+require('dotenv').config();
+
 
 const app = express();
 
@@ -45,7 +47,7 @@ function verifyToken(req, res, next) {
     req.token = bearerToken;
     jwt.verify(
       req.token,
-      "2792e2ad90d216092e08cf7e70b23fdcd84fbd757343f982fc0d3f5f27f1034bd99b07049e6002c662acb454cef7aeeecdaf028b8250c95f3ef78ca250a4828fc206cc9606cee4754f7d560744811d2dcbfac33a55643062d62cecf8f103b5d51f7b763c1977181e27c9762782bee835822c72df5283431b50678a9591c03892",
+      process.env.SECRET_KEY,
       (err, authData) => {
         if (err) {
           res.sendStatus(403);
